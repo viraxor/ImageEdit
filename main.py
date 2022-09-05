e5070efe1e51e847502ca72bd0aa2332fa296186
viraxor <55741238+viraxor@users.noreply.github.com>
2022-09-05 10:47:26 +0200

    new stuff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageTk, ImageStat, UnidentifiedImageError
 import tkinter as tk
 from tkinter import messagebox, colorchooser, filedialog, ttk
 import effects, generators, macrocreator, kernelcreator
+import pickpoint
 from inspect import getmembers, isfunction
 import glob
 
@@ -11,7 +12,7 @@ class App():
         
         self.max_image_pixels_number = Image.MAX_IMAGE_PIXELS
 
-        self.version = "v1.2.2"
+        self.version = "v1.2.1"
         
         self.root = tk.Tk()
         self.root.title(f"ImageEdit {self.version}")
@@ -249,7 +250,6 @@ class App():
                 if not param.startswith("image"):
                     self.sliders.append(tk.Scale(self.choose_repeat_options, from_=0, to=generators._limit(self.choose_repeat_actual_var.get(), param), orient="horizontal"))
                     self.labels.append(tk.Label(self.choose_repeat_options, text=param))
-
 
     def repeat_menu_quit(self):
         self.current_image = self.last_image
@@ -631,65 +631,15 @@ class App():
         self.checkbutton_frame.grid(row=2, column=0)
         
         self.resize_window.mainloop()
-        
-    def return_selected_point(self):
-        if self.selected_point == None:
-            messagebox.showerror(message="You must select a point first!", title="Error")
-        else:
-            self.select_point_window.destroy()
-            self.select_point_window.quit()
-            
-    def return_no_point(self):
-        self.selected_point = None
-        self.select_point_window.destroy()
-        self.select_point_window.quit()
-    
-    def locate_selected_point(self, event):
-        self.show_image_x_point = self.select_point_image.winfo_pointerx() - self.select_point_image.winfo_rootx()
-        self.show_image_y_point = self.select_point_image.winfo_pointery() - self.select_point_image.winfo_rooty()
-        
-        # this might have some inaccuracies
-        self.current_image_x_point = round(self.show_image_x_point * self.modify_percent)
-        self.current_image_y_point = round(self.show_image_y_point * self.modify_percent)
-        
-        self.selected_point = (self.current_image_x_point, self.current_image_y_point)
-        
-        self.select_point_label.configure(text=f"Point located! The location is: {self.selected_point}")
-        
-    def select_point(self, point_description):
-        self.select_point_window = tk.Toplevel(self.root)
-        self.select_point_window.title(f"Selecting the {point_description} point")
-        
-        self.select_point_label = tk.Label(self.select_point_window, text=f"Select the {point_description} point:")
-        self.select_point_image = tk.Label(self.select_point_window, image=self.show_image)
-        
-        self.select_point_buttons = tk.Frame(self.select_point_window)
-        
-        self.select_point_button_ok = tk.Button(self.select_point_buttons, text="OK", command=self.return_selected_point)
-        self.select_point_button_cancel = tk.Button(self.select_point_buttons, text="Cancel", command=self.return_no_point)
-        
-        self.select_point_button_ok.grid(row=0, column=0)
-        self.select_point_button_cancel.grid(row=0, column=1)
-        
-        self.select_point_label.grid(row=0, column=0)
-        self.select_point_image.grid(row=1, column=0)
-        
-        self.select_point_buttons.grid(row=2, column=0)
-        
-        self.selected_point = None
-        
-        self.select_point_image.bind("<Button-1>", self.locate_selected_point)
-        
-        self.select_point_window.mainloop()
-        
+                
     def crop_menu(self):
-        self.select_point("top left")
-        self.topleft_point = self.selected_point
+        self.topleft_point = pickpoint.Window(self.root, self.show_image, self.modify_percent, "top left")
+        self.topleft_point = self.topleft_point.selected_point
         if self.topleft_point == None:
             return None
             
-        self.select_point("bottom right")
-        self.bottomright_point = self.selected_point
+        self.bottomright_point = pickpoint.Window(self.root, self.show_image, self.modify_percent, "bottom right")
+        self.bottomright_point = self.bottomright_point.selected_point
         if self.bottomright_point == None:
             return None
         
